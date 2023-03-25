5e0931061446ec767d7c9c9eb38130d45f2206c5
arin2115 <karciello.yt@gmail.com>
2023-03-25 14:13:02 +0100

    remove debug

--- utils/timers.js
+++ utils/timers.js
@@ -28,9 +28,6 @@ async function editTimer(timerData) {
         .then(async (data) => {
             var timer = data.find(t => t.id == timerData.id);
 
-            console.log(timerData)
-            console.log(data)
-
             await db.pull(`timers`, t => t.id == timerData.id);
 
             var newTimer = {
